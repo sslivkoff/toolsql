e7f7d02d38e10a70b19a1bfd253276dd7db7e70b
sslivkoff <stormslivkoff@gmail.com>
2023-02-03 17:34:09 -0800

    update tx planning

--- toolsql/drivers/conn_utils/transaction_utils.py
+++ toolsql/drivers/conn_utils/transaction_utils.py
@@ -1,12 +1,42 @@
+# from __future__ import annotations
+
+# from toolsql import spec
+
+# """
+
+# 1.
+#     with toolsql.connect(db_config) as conn:
+#         with toolsql.begin(conn):
+#             ...
+#             toolsql.rollback(conn)
+
+# 2.
+#     conn = toolsql.connect(db_config)
+#     with toolsql.begin(conn):
+#         ...
+#         toolsql.rollback(conn)
+
+# 3.
+#     conn = toolsql.connect(db_config)
+#     toolsql.begin(conn)
+#     ...
+#     toolsql.commit(conn) | toolsql.rollback(conn)
+
+# 4.
+#     with toolsql.connect(db_config) as conn:
+#         toolsql.begin(conn)
+#         ...
+#         toolsql.commit(conn) | toolsql.rollback(conn)
+
+
+# """
+
+
 # def begin(
+#     conn: spec.Connection,
 #     uri: str | None = None,
 #     db_config: spec.DBConfig | None = None,
 # ) -> spec.Transaction:
-
-#     if uri is None:
-#         if db_config is None:
-#             raise Exception('must specify uri or db_config')
-#         uri = get_db_uri(db_config=db_config)
 #     driver = drivers.get_driver(uri=uri, db_config=db_config, sync=True)
 #     return driver.begin(uri=uri)
 
