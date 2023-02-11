734a2e50894993ae91d348963ec16327dc0cc710
sslivkoff <stormslivkoff@gmail.com>
2023-02-11 11:13:18 -0800

    add module-level docstring and version

--- toolsql/__init__.py
+++ toolsql/__init__.py
@@ -1,5 +1,10 @@
+"""toolsql is an async+sync sql builder+executor for sqlite+postgres"""
+
 from .drivers import *
 from .executors import *
 from .schemas import *
 from .spec import *
 from .statements import *
+
+__version__ = '0.4.0'
+
