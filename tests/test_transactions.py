e37ceaccaed119e3f75240b90bfde19f02c4f07d
sslivkoff <stormslivkoff@gmail.com>
2023-03-04 19:23:41 -0800

    add transaction_utils

---
+++ tests/test_transactions.py
@@ -0,0 +1,118 @@
+import polars as pl
+import pytest
+
+import toolsql
+import conf.conf_tables as conf_tables
+
+
+test_tables = conf_tables.get_test_tables()
+
+pokemon = test_tables['pokemon']
+pokemon_columns = list(pokemon['schema']['columns'].keys())
+polars_pokemon = pl.DataFrame(pokemon['rows'], schema=pokemon_columns)
+polars_pokemon = polars_pokemon.with_columns(
+    pl.Series(
+        values=[list(item) for item in polars_pokemon['all_types']],
+        dtype=pl.Object,
+        name='all_types',
+    )
+)
+
+
+class CustomInnerException(Exception):
+    pass
+
+
+def test_sync_tx_fail(sync_write_db_config, fresh_pokemon_table, helpers):
+
+    db_config = sync_write_db_config
+    schema = fresh_pokemon_table['schema']
+    rows = fresh_pokemon_table['rows']
+
+    # create table
+    with toolsql.connect(db_config) as conn:
+        toolsql.create_table(table=schema, conn=conn, confirm=True)
+
+    # insert rows in failed transaction
+    with pytest.raises(CustomInnerException):
+        with toolsql.connect(db_config, autocommit=True) as conn:
+            with toolsql.begin(conn):
+                toolsql.insert(table=schema, rows=rows[:5], conn=conn)
+                raise CustomInnerException()
+
+    # confirm table full
+    with toolsql.connect(db_config) as conn:
+        result = toolsql.select(
+            table=schema,
+            conn=conn,
+            columns=['COUNT(*)'],
+            output_format='cell',
+        )
+        assert result == 0
+
+    # insert rows in non-failed transaction
+    with pytest.raises(CustomInnerException):
+        with toolsql.connect(db_config, autocommit=True) as conn:
+            with toolsql.begin(conn):
+                toolsql.insert(table=schema, rows=rows[:5], conn=conn)
+            raise CustomInnerException()
+
+    # confirm table full
+    with toolsql.connect(db_config) as conn:
+        result = toolsql.select(
+            table=schema,
+            conn=conn,
+            columns=['COUNT(*)'],
+            output_format='cell',
+        )
+        assert result == 5
+
+
+async def test_async_tx_fail(
+    async_write_db_config, fresh_pokemon_table, helpers
+):
+
+    db_config = async_write_db_config
+    sync_db_config = db_config.copy()
+    del sync_db_config['driver']
+    schema = fresh_pokemon_table['schema']
+    rows = fresh_pokemon_table['rows']
+
+    # create table
+    with toolsql.connect(sync_db_config) as conn:
+        toolsql.create_table(table=schema, conn=conn, confirm=True)
+
+    # insert rows in failed transaction
+    with pytest.raises(CustomInnerException):
+        async with toolsql.async_connect(db_config, autocommit=True) as conn:
+            async with toolsql.async_begin(conn):
+                await toolsql.async_insert(
+                    table=schema, rows=rows[:5], conn=conn
+                )
+                raise CustomInnerException()
+
+    # confirm table full
+    async with toolsql.async_connect(db_config) as conn:
+        result = await toolsql.async_select(
+            table=schema,
+            conn=conn,
+            columns=['COUNT(*)'],
+            output_format='cell',
+        )
+        assert result == 0
+
+    # insert rows alongside Exception
+    async with toolsql.async_connect(db_config, autocommit=True) as conn:
+        async with toolsql.async_begin(conn):
+            await toolsql.async_insert(table=schema, rows=rows[:5], conn=conn)
+
+    # confirm table full
+    async with toolsql.async_connect(db_config) as conn:
+        result = await toolsql.async_select(
+            table=schema,
+            conn=conn,
+            columns=['COUNT(*)'],
+            output_format='cell',
+        )
+        assert result == 5
+
--- toolsql/drivers/conn_utils/transaction_utils.py
+++ toolsql/drivers/conn_utils/transaction_utils.py
@@ -1,6 +1,12 @@
-# from __future__ import annotations
+from __future__ import annotations
 
-# from toolsql import spec
+import typing
+
+from toolsql import spec
+from .. import driver_utils
+
+if typing.TYPE_CHECKING:
+    import psycopg
 
 # """
 
@@ -32,24 +38,121 @@
 # """
 
 
-# def begin(
-#     conn: spec.Connection,
-#     uri: str | None = None,
-#     db_config: spec.DBConfig | None = None,
-# ) -> spec.Transaction:
-#     driver = drivers.get_driver(uri=uri, db_config=db_config, sync=True)
-#     return driver.begin(uri=uri)
+class _TransactionContext:
+    def __init__(self, conn: spec.Connection) -> None:
+        self.conn = conn
+        self.driver = driver_utils.get_driver_name(conn=conn)
+
+        if self.driver == 'sqlite3':
+            conn.execute('BEGIN')
+        elif self.driver in ['psycopg']:
+            pass
+        else:
+            raise Exception('invalid driver')
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exception_type, exception_value, exception_traceback):
+        if self.driver == 'sqlite3':
+            if exception_type is None:
+                self.conn.execute('COMMIT')
 
 
-# def begin_async(
-#     uri: str | None = None,
-#     db_config: spec.DBConfig | None = None,
-# ) -> spec.AsyncTransaction:
+class _AsyncTransactionContext:
+    def __init__(self, conn: spec.Connection) -> None:
+        self.conn = conn
+        self.driver = driver_utils.get_driver_name(conn=conn)
 
-#     if uri is None:
-#         if db_config is None:
-#             raise Exception('must specify uri or db_config')
-#         uri = get_db_uri(db_config=db_config)
-#     driver = drivers.get_driver(uri=uri, db_config=db_config, sync=True)
-#     return driver.begin_async(uri=uri)
+        if self.driver == 'aiosqlite':
+            pass
+        elif self.driver in ['psycopg']:
+            pass
+        else:
+            raise Exception('invalid driver')
+
+    async def __aenter__(self):
+        if self.driver == 'aiosqlite':
+            await self.conn.execute('BEGIN')
+        elif self.driver in ['psycopg']:
+            pass
+        else:
+            raise Exception('invalid driver')
+
+    async def __aexit__(
+        self, exception_type, exception_value, exception_traceback
+    ):
+        if self.driver == 'aiosqlite':
+            if exception_type is None:
+                await self.conn.execute('COMMIT')
+
+
+def begin(conn: spec.Connection) -> _TransactionContext | psycopg.Transaction:
+    driver = driver_utils.get_driver_name(conn=conn)
+    if driver == 'sqlite3':
+        return _TransactionContext(conn=conn)
+    elif driver == 'psycopg':
+        return conn.transaction()
+    else:
+        raise Exception('invalid driver')
+
+
+def commit(conn: spec.Connection) -> None:
+
+    driver = driver_utils.get_driver_name(conn=conn)
+    if driver == 'sqlite3':
+        conn.execute('BEGIN')
+    elif driver == 'psycopg':
+        conn.commit()
+    else:
+        raise Exception('invalid driver')
+
+
+def rollback(conn: spec.Connection) -> None:
+
+    driver = driver_utils.get_driver_name(conn=conn)
+    if driver == 'sqlite3':
+        conn.execute('ROLLBACK')
+    elif driver == 'psycopg':
+        conn.rollback()
+    else:
+        raise Exception('invalid driver')
+
+
+#
+# # async versions
+#
+
+def async_begin(
+    conn: spec.Connection,
+) -> _AsyncTransactionContext | psycopg.Transaction:
+    driver = driver_utils.get_driver_name(conn=conn)
+    if driver == 'aiosqlite':
+        return _AsyncTransactionContext(conn=conn)
+    elif driver == 'psycopg':
+        return conn.transaction()
+    else:
+        raise Exception('invalid driver')
+
+
+async def async_commit(conn: spec.Connection) -> None:
+
+    driver = driver_utils.get_driver_name(conn=conn)
+    if driver == 'sqlite3':
+        await conn.execute('COMMIT')
+    elif driver == 'psycopg':
+        await conn.commit()
+    else:
+        raise Exception('invalid driver')
+
+
+async def async_rollback(conn: spec.Connection) -> None:
+
+    driver = driver_utils.get_driver_name(conn=conn)
+    if driver == 'sqlite3':
+        await conn.execute('ROLLBACK')
+    elif driver == 'psycopg':
+        await conn.rollback()
+    else:
+        raise Exception('invalid driver')
 
