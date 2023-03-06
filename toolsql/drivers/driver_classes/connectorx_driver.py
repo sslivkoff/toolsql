5196404cd4713f0b4147aeb15d1e32115f4a92d6
sslivkoff <stormslivkoff@gmail.com>
2023-03-05 18:34:40 -0800

    remove verbose error printout

--- toolsql/drivers/driver_classes/connectorx_driver.py
+++ toolsql/drivers/driver_classes/connectorx_driver.py
@@ -169,8 +169,6 @@ class ConnectorxDriver(abstract_driver.AbstractDriver):
         try:
             result = connectorx.read_sql(conn, sql, return_type=result_format)
         except Exception as e:
-            print('before', type(e), e,)
-            print('after', type(spec.convert_exception(e)), spec.convert_exception(e))
             raise spec.convert_exception(e)
         result = formats.decode_columns(rows=result, columns=decode_columns)
         return formats.format_row_dataframe(result, output_format=output_format)
