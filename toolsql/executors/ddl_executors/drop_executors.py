634c50426cf97880d6cfe0f45832bee0593259fa
sslivkoff <stormslivkoff@gmail.com>
2023-03-28 11:19:30 -0700

    add more table drop functions

--- toolsql/executors/ddl_executors/drop_executors.py
+++ toolsql/executors/ddl_executors/drop_executors.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import typing
+
 from toolsql import spec
 from toolsql import statements
 
@@ -11,10 +13,46 @@ def drop_table(
     confirm: bool = False,
     if_exists: bool = False,
 ) -> None:
+    """drop table from database"""
 
     if not confirm:
         raise Exception('to drop table use confirm=True')
 
     sql = statements.build_drop_table_statement(table, if_exists=if_exists)
     conn.execute(sql)
+
+
+def drop_tables(
+    *,
+    tables: typing.Sequence[str],
+    if_exists: bool = False,
+    conn: spec.Connection,
+    confirm: bool = False,
+) -> None:
+    """drop tables from database"""
+
+    for table in tables:
+        drop_table(
+            table=table,
+            conn=conn,
+            confirm=confirm,
+            if_exists=if_exists,
+        )
+
+
+def drop_schema_tables(
+    db_schema: spec.DBSchema,
+    *,
+    conn: spec.Connection,
+    confirm: bool = False,
+    if_exists: bool = False,
+) -> None:
+    """drop tables found in schema"""
+
+    drop_tables(
+        tables=list(db_schema['tables'].keys()),
+        conn=conn,
+        if_exists=if_exists,
+        confirm=confirm,
+    )
 
